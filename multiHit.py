ebf790ab61b226b1f458443d48f182eab30e7ecf
oOHiyoriOo <44544317+oOHiyoriOo@users.noreply.github.com>
2021-05-04 12:40:43 +0200

    Improovements, Works best on Linux !

--- multiHit.py
+++ multiHit.py
@@ -6,11 +6,12 @@ import threading
 import requests
 import os
 from colorama import Fore,init
+import platform
 
 init()
 
 __dirname = os.path.abspath(os.path.dirname(__file__))
-RDIR = ["lists","logs"]
+RDIR = ["logs"]
 for DIR in RDIR:
     if not os.path.isdir(__dirname+'/'+DIR):
         os.mkdir(__dirname+'/'+DIR)
@@ -19,14 +20,18 @@ s_print_lock = threading.Lock()
 def s_print(*a, **b):
     """Thread safe print function"""
     with s_print_lock:
-        s_print("\033[%d;%dH" % (0, 0))
         print(*a, **b)
 
 
 def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
     fails = 0
     i = 0
-    spacer = "\n"*(ThreadNo+1)
+
+    if str(platform.system() == "Linux"):
+        spaces = "\033[0H" + ("\n"*(ThreadNo+1))
+    else:
+        spaces = ""
+
     while i < ProxyCount:
         try:
             s = requests.get("https://public.freeproxyapi.com/api/Proxy/Mini")
@@ -38,7 +43,7 @@ def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
                 if ProxyList.count(proxy) == 0:
                     ProxyList.append(proxy)
                     i = i + 1
-                    s_print(spacer+Fore.GREEN+"Thread["+str(ThreadNo)+"] Got["+str(i)+"]: "+str(res['host'])+":"+str(res['port']))
+                    s_print(spaces+Fore.GREEN+"Thread["+str(ThreadNo)+"] Got["+str(i)+"]: "+str(res['host'])+":"+str(res['port'])+(" "*10))
 
             elif fails >= MaxRequestFails:
                 i = ProxyCount
@@ -46,17 +51,18 @@ def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
 
         except Exception as err:
             fails = fails + 1
-            s_print(spacer+Fore.RED+"Thread["+str(ThreadNo)+"] Failed."+Fore.RESET)
+            s_print(Fore.RED+"Thread["+str(ThreadNo)+"] Failed."+Fore.RESET)
             LOGFILE = __dirname+"/logs/Thread["+str(ThreadNo)+"].log"
             MODE = 'a' if os.path.isfile(LOGFILE) else 'w'
             with open(LOGFILE,MODE,encoding='utf-8') as log:
                 log.write(str(err)+"\n")
-    
+
+    s_print(spaces+Fore.YELLOW+"Thread["+str(ThreadNo)+"] Finished Job."+(" "*20))
 
 
 if __name__ == '__main__':
-    Threads = 20
-    ProxyLoopCount = 100000
+    Threads = 10
+    ProxyLoopCount = 10000
     MaxFails = 2
 
     ProxyListShared = []
@@ -75,3 +81,5 @@ if __name__ == '__main__':
     with open('SyncList.txt',MODE,encoding='utf-8') as FinalSync:
         for proxy in ProxyListShared:
             FinalSync.write(proxy+"\n")
+
+    print(Fore.RESET+"\n"*(Threads+1))
