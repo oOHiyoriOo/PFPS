824c000510dfe03eaa3ce7b6f7e294f96ee1bb62
oOHiyoriOo <44544317+oOHiyoriOo@users.noreply.github.com>
2021-05-16 17:55:40 +0200

    removed unused import, filter for alive proxys only

--- multiHit.py
+++ multiHit.py
@@ -1,6 +1,4 @@
-import multiprocessing as mp
-from ctypes import c_char_p
-###################################
+
 import threading
 
 import requests
@@ -23,7 +21,7 @@ def s_print(*a, **b):
         print(*a, **b)
 
 
-def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
+def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails,PingCheck):
     fails = 0
     i = 0
 
@@ -34,20 +32,27 @@ def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
 
     while i < ProxyCount:
         try:
-            s = requests.get("https://public.freeproxyapi.com/api/Proxy/Mini")
+            s = requests.get("https://public.freeproxyapi.com/api/Proxy/Medium")
 
             if s.status_code == 200:
                 res = s.json()
+                
+                if res['isAlive'] == True and res['proxyLevel'] in ["Anonymous","Elite"]: # just skip the proxy if it's considered 'dead', i dont value this as error.
+                    proxy = str(res['host'])+":"+str(res['port'])
+                    if ProxyList.count(proxy) == 0:
+                        if PingCheck:
+                            pass
 
-                proxy = str(res['host'])+":"+str(res['port'])
-                if ProxyList.count(proxy) == 0:
-                    ProxyList.append(proxy)
-                    i = i + 1
-                    s_print(spaces+Fore.GREEN+"Thread["+str(ThreadNo)+"] Got["+str(i)+"]: "+str(res['host'])+":"+str(res['port'])+(" "*10))
+                        ProxyList.append(proxy)
+                        i = i + 1
+                        s_print(spaces+Fore.GREEN+"Thread["+str(ThreadNo)+"] Got["+str(i)+"]: "+str(res['host'])+":"+str(res['port'])+(" "*10))
+
 
             elif fails >= MaxRequestFails:
                 i = ProxyCount
                 break
+            elif s.status_code != 200:
+                raise ConnectionError("s.status_code: "+str(s.status_code))
 
         except Exception as err:
             fails = fails + 1
@@ -61,15 +66,17 @@ def GetProxy(ThreadNo,ProxyList,ProxyCount,MaxRequestFails):
 
 
 if __name__ == '__main__':
-    Threads = 40
-    ProxyLoopCount = 10000
+    Threads = 20
+    ProxyLoopCount = 5000
     MaxFails = 2
+    PingCheck = True
+
 
     ProxyListShared = []
     TasksList = []
     
     for i in range(Threads):
-        CT = threading.Thread(target=GetProxy, args=(i,ProxyListShared,int(round(ProxyLoopCount / Threads,0)),MaxFails,),daemon=True )
+        CT = threading.Thread(target=GetProxy, args=(i,ProxyListShared,int(round(ProxyLoopCount / Threads,0)),MaxFails,PingCheck,),daemon=True )
         TasksList.append(CT)
         CT.start()
 
@@ -82,10 +89,10 @@ if __name__ == '__main__':
     else:
         spaces = ""
 
-    print(spaces+"List: "+str(len(ProxyListShared))+"/"+str(ProxyLoopCount))
+    print(spaces+"Result: "+str(len(ProxyListShared))+"/"+str(ProxyLoopCount)+" Proxys saved.")
     MODE = 'a' if os.path.isfile('SyncList.txt') else 'w'
     with open('SyncList.txt',MODE,encoding='utf-8') as FinalSync:
         for proxy in ProxyListShared:
             FinalSync.write(proxy+"\n")
 
-    print(spaces+Fore.RESET+"\n"*(Threads+2))
+    print("\n"+spaces+Fore.RESET)
